6fc86c29a93f631224cb137c213044ecb47bb539
DiamondHands89 <kwamemullins@outlook.com>
2025-02-27 04:47:52 -0500

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar" style={{ padding: '1rem', backgroundColor: '#333' }}>
+      <Link to="/" style={{ color: '#fff', marginRight: '1rem' }}>Home</Link>
+      <Link to="/adventure" style={{ color: '#fff', marginRight: '1rem' }}>Adventure</Link>
+      <Link to="/dashboard" style={{ color: '#fff', marginRight: '1rem' }}>Dashboard</Link>
+      <Link to="/upgrade" style={{ color: '#fff', marginRight: '1rem' }}>Upgrade</Link>
+    </nav>
+  );
 };
 
 export default Navbar;
